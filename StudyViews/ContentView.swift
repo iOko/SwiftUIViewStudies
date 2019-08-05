85b4b3968ed603a23254523cfa5d4e2df95a1d91
Kyle Ozaltug <kozaltug@gmail.com>
2019-08-05 15:01:48 -0400

    Initial Commit

---
+++ StudyViews/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  StudyViews
+//
+//  Created by Kyle Ozaltug on 2019-08-05.
+//  Copyright © 2019 Kyle Ozaltug. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
